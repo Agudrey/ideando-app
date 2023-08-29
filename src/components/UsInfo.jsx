03205e254fbb2e4ffa4da5258c78c2f8169a367e
Agudrey <cristianagudelorey1@gmail.com>
2023-08-28 20:19:24 -0500

    responsive changes

--- src/components/UsInfo.jsx
+++ src/components/UsInfo.jsx
@@ -2,13 +2,17 @@ import "./css/UsInfo.css"
 import UsInfoCard from "./subcomponents/UsInfoCard"
 import UsInfoExperience from "./subcomponents/UsInfoExperience"
 import UsInfoLine from "./subcomponents/UsInfoLine"
-
+import Zoom from 'react-reveal/Zoom';
 
 function UsInfo() {
   return (
     <section className="info-container" id="usInfo" style={{backgroundImage: `url('./interface-images/us-info-image.webp')`}}>
       <div className="info-container-background-color"></div>
-      <h1>¡Creamos el evento perfecto para ti!</h1>
+      <h1>
+        <Zoom cascade>
+          ¡Creamos el evento perfecto para ti!
+        </Zoom>
+      </h1>
 
       <UsInfoExperience />
 
--- src/components/subcomponents/UsInfoExperience.jsx
+++ src/components/subcomponents/UsInfoExperience.jsx
@@ -1,7 +1,8 @@
 import "./css/UsInfoExperience.css"
 import { RiMedalLine } from 'react-icons/ri';
 import { BsFillPatchCheckFill } from 'react-icons/bs';
-
+import Slide from 'react-reveal/Slide';
+import Zoom from 'react-reveal/Zoom';
 
 function UsInfoExperience() {
 
@@ -25,28 +26,34 @@ function UsInfoExperience() {
   return (
     <section className="experience-container">
       <div className="info-experience-container">
-        <div className="years-experience-container">
-          <div>
-            <RiMedalLine className="experience-icon"/>
+        <Slide left>
+          <div className="years-experience-container">
+            <div>
+              <RiMedalLine className="experience-icon"/>
 
-            <h3 className="experience-years">8</h3>
-            <p key={1} className="experience-years-text">Años de experiencia en el sector</p>
+              <h3 className="experience-years">8</h3>
+              <p key={1} className="experience-years-text">Años de experiencia en el sector</p>
+            </div>
+            
+            <ul className="experience-points">
+              {experiencePoints.map((item) =>
+                <li key={item.id}><BsFillPatchCheckFill className="points-icons"/> {item}</li>
+              )}
+            </ul>
           </div>
-          
-          <ul className="experience-points">
-            {experiencePoints.map((item) =>
-              <li key={item.id}><BsFillPatchCheckFill className="points-icons"/> {item}</li>
-            )}
-          </ul>
-        </div>
+        </Slide>
 
-        <article className="experience-description">
-          <p>Ideando Eventos y Marketing es una Empresa que cuenta con la experiencia de más de 8 años en la organización integral de diseño, logística, producción e innovación corporativa para empresas. Garantizamos que las marcas cobren vida a través de actividades únicas, innovadoras y útiles para las empresas. Por ello te ofrecemos un equipo de trabajo apasionado, comprometido y profesional quienes son los encargados de cada detalle; Apostamos por el asesoramiento, la atención, la creatividad, el trabajo en equipo, el compromiso y  la responsabilidad de cada Evento.</p>
-        </article>
+        <Slide right>
+          <article className="experience-description">
+            <p>Ideando Eventos y Marketing es una Empresa que cuenta con la experiencia de más de 8 años en la organización integral de diseño, logística, producción e innovación corporativa para empresas. Garantizamos que las marcas cobren vida a través de actividades únicas, innovadoras y útiles para las empresas. Por ello te ofrecemos un equipo de trabajo apasionado, comprometido y profesional quienes son los encargados de cada detalle; Apostamos por el asesoramiento, la atención, la creatividad, el trabajo en equipo, el compromiso y  la responsabilidad de cada Evento.</p>
+          </article>
+        </Slide>
       </div>
 
       <div className="experience-button-container">
-        <button onClick={() => clickScroll("contact")}>Quiero Hacer un evento</button>
+        <Zoom>
+          <button onClick={() => clickScroll("contact")}>Quiero Hacer un evento</button>
+        </Zoom>
       </div>
     </section>
   )
